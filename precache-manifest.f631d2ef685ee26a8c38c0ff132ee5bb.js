7dc777e64ad9142e196720c658ca9457af7e58a5
Junruo <995582363@qq.com>
2019-09-28 18:15:01 -0700

    Updates

---
+++ precache-manifest.f631d2ef685ee26a8c38c0ff132ee5bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9790e7b20c3fcfb67adf",
+    "url": "/static/css/main.79ebdc11.chunk.css"
+  },
+  {
+    "revision": "9790e7b20c3fcfb67adf",
+    "url": "/static/js/main.9790e7b2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "08c60dd8951bb0dd0036",
+    "url": "/static/js/2.08c60dd8.chunk.js"
+  },
+  {
+    "revision": "cfde6996b1f8012885f91734ac711508",
+    "url": "/static/media/home_hero2.cfde6996.jpg"
+  },
+  {
+    "revision": "71d3575549c248bc436c122a0e4d5616",
+    "url": "/static/media/ste.71d35755.jpg"
+  },
+  {
+    "revision": "e4bbba7aa0f7f413697fb9c9889750ce",
+    "url": "/index.html"
+  }
+];
