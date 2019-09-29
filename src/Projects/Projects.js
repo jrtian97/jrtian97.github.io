01abbfa06622af53a7cefca27f03489839a88777
Junruo <995582363@qq.com>
2019-09-28 20:42:37 -0700

    add new project

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -7,10 +7,23 @@ import Project from '../Component/Project/Project'
 import ste from './ste.jpg'
 
 // Project details
+const projectEE5 = {
+  description: {
+    role: 'Software/Hardware Engineer',
+    technique: 'MPLAB / C',
+    job: [
+      'Implemented Proportional Integral Differential control algorithm to control motor speed on bike trailer',
+      'Researched and tested strain gauge sensor to measure the strain between bike and trailer',
+      'Input the strain voltage to the computer by microprocessor and circuit',
+      'Processed strain voltage to control the speed of bike trailer through Pulse Width Modulation'
+    ]
+  }
+}
+
 const projectEE4 = {
   videoId: 'D9wHb0TmCMo',
   description: {
-    role: 'Leader',
+    role: 'Lead Software Engineer',
     technique: 'EAGLE / MPLAB / C',
     job: [
       'Designed and welded the PCB for the screen',
@@ -23,7 +36,7 @@ const projectEE4 = {
 const projectEE3 = {
   videoId: 'a-d40GZPZQk',
   description: {
-    role: 'Member',
+    role: 'Software Engineer',
     technique: 'Fischertechnik / LabVIEW',
     job: [
       'Selected, assembled and welded metal sensor as well as infrared photoelectric sensor',
@@ -35,7 +48,7 @@ const projectEE3 = {
 const projectAndroid = {
   videoId: 'C0IS7boZsIk',
   description: {
-    role: 'Member',
+    role: 'Software Engineer',
     technique: 'JAVA / Android Studio',
     job: [
       'Defined user needs, determined app functions, and designed concise user interfaces',
@@ -46,7 +59,7 @@ const projectAndroid = {
 
 const projectSTE = {
   description: {
-    role: 'Leader',
+    role: 'Project Manager',
     technique: 'SWOT / BDR / MDR',
     job: [
       'Analyzed marketing micro and macro environment, segmented consumers and business market and identified target groups',
@@ -71,9 +84,27 @@ class Projects extends Component {
     return (
       <div className="projects">
         <section className="project">
+          <h3 className="project-title">Smart Bike Trailer</h3>
+          <p>
+            An smart(self-propelled) bike trailer to help rider not feel
+            anything when the trailer carry 60kg objects
+          </p>
+          <Project detail={projectEE5.description} />
+          <div>
+            <a
+              className="project-link"
+              href="https://kuleuven.mediaspace.kaltura.com/media/demo_movie/1_72ucgniu"
+              target="_blank"
+            >
+              Demo Link
+            </a>
+          </div>
+        </section>
+
+        <section className="project">
           <h3 className="project-title">Catch The Thief</h3>
           <p>
-            An Electronic Laser Game. The player sets obstacles (shoot laser
+            An electronic laser game. The player sets obstacles (shoot laser
             beams on LDRs) on a screen for a thief (flashing alternately
             red-green LED) with a 3D Printed laser gun to get a score
           </p>
@@ -82,6 +113,7 @@ class Projects extends Component {
             <YouTube videoId={projectEE4.videoId} opts={opts} />
           </div>
         </section>
+
         <section className="project">
           <h3 className="project-title">Smart Trash Can</h3>
           <p>
@@ -95,6 +127,7 @@ class Projects extends Component {
             <YouTube videoId={projectEE3.videoId} opts={opts} />
           </div>
         </section>
+
         <section className="project">
           <h3 className="project-title">What To Wear</h3>
           <p>
@@ -107,6 +140,7 @@ class Projects extends Component {
             <YouTube videoId={projectAndroid.videoId} opts={opts} />
           </div>
         </section>
+
         <section className="project">
           <h3 className="project-title">Magnetic Chessboard Wallpaper</h3>
           <p>
